d00a746b72b2682ba5ff2071c1b1026873a11026
Duong Le <duongbk1602@gmail.com>
2023-02-21 22:52:50 +0700

    logging (cont)

--- trainer.py
+++ trainer.py
@@ -500,6 +500,9 @@ class Trainer(object):
         for _ in range(self.aft_seq_length):
             output = self.G(cur_seq)[:,:1]
             pred_y.append(output)
+
+            print(cur_seq.clone()[:,1:].shape)
+            print(output.clone().shape)
             cur_seq = torch.vstack((cur_seq.clone()[:,1:], output.clone()))
 
         pred_y = torch.cat(pred_y, dim=1)
